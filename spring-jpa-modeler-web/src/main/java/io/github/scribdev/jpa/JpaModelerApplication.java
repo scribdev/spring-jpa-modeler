620191a6fd965c3bf1a0188fc8d68f08725b5dec
Michael Halwax <mhalwax@gmail.com>
2020-04-19 17:54:57 +0200

    Default Application for tests

---
+++ spring-jpa-modeler-web/src/main/java/io/github/scribdev/jpa/JpaModelerApplication.java
@@ -0,0 +1,17 @@
+package io.github.scribdev.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class,
+		DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
+public class JpaModelerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaModelerApplication.class, args);
+	}
+
+}
