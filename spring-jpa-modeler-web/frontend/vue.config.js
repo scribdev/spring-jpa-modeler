808af47a6fac4e35e8015db667981a7cfd261bcd
Michael Halwax <mhalwax@gmail.com>
2020-04-19 18:37:55 +0200

    configure devserver and api-proxy

--- spring-jpa-modeler-web/frontend/vue.config.js
+++ spring-jpa-modeler-web/frontend/vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  devServer: {
+    port: 3000,
+    proxy: {
+      '/api/*': {
+        target: 'http://localhost:8080',
+      },
+    },
+  },
   transpileDependencies: [
     'vuetify',
   ],
